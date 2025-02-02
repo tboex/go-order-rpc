54b753ec7751810dc9b9fab0446be79bebb07c2b
Tim Boex <timboex@gmail.com>
2025-02-02 08:52:24 -0600

    feature/ changed deprecated Dial to use NewClient

--- client/client.go
+++ client/client.go
@@ -9,10 +9,11 @@ import (
 	orderpb "order-rpc/api/proto"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Failed to connect: %v", err)
 	}
